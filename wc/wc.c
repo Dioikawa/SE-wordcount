fc85bb355de620759a0ff7095fc60302f85efa72
Dioikawa <55572810+Dioikawa@users.noreply.github.com>
2020-03-15 22:15:46 +0800

    扩展功能

--- wc/wc.c
+++ wc/wc.c
@@ -50,12 +50,129 @@ void LineCount(FILE *f)  //计算文件的行数目 -l
 		while(!feof(f))
 		{
 			l=fgetc(f);
-			if(l=='\n'||l=='\t')
+			if(l=='\n')
 				count++;
 		}
 	}
 	fclose(f);
 	printf("The number of line is %d.\n",count);
+}
+
+void BlankLineCount(FILE *f)   //计算空白行
+{
+	char b;
+	int blankline=0;     //统计空白行数
+	int chcount=0;       //统计字符个数
+	while(!feof(f))
+	{
+		b=fgetc(f);
+		if(0==chcount)
+		{
+			if(b=='\n')
+				blankline++;
+			else if(b!=' '&&b!='\t')
+				chcount++;
+		}
+		else if(1==chcount)
+		{
+			if(b=='\n')
+			{
+				blankline++;
+				chcount=0;
+			}
+			else if(b!=' '&&b!='\t')
+				chcount++;
+		}
+		else
+		{
+			if(b=='\n')
+				chcount=0;
+		}
+	}
+	if(chcount<=1)           //判断最后一行是否为空白行
+		blankline++;
+	fclose(f);
+	printf("The number of blankline is %d.\n",blankline);
+}
+
+void CodeLineCount(FILE *f)    //计算代码行
+{
+	char c;
+	int chcount=0;
+	int codeline=0;
+	while(!feof(f))
+	{
+		c=fgetc(f);
+		if(chcount<=1)
+		{
+			if(c=='\n')
+				chcount=0;
+			else if(c!=' '&&c!='\t')
+				chcount++;
+		}
+		else
+		{
+			if(c=='\n')
+			{
+				codeline++;
+				chcount=0;
+			}
+		}
+	}
+	if(chcount<=1)           //判断最后一行是否为代码行
+		codeline++;
+	fclose(f);
+	printf("The number of codeline is %d.\n",codeline);
+}
+
+void CommentLineCount(FILE *f) //计算单行注释行的行数
+{
+	char c;
+	int chcount=0;       //字符数
+	int commentline=0;   //注释行数
+	int tag=0;           //tag为1表示该行为注释行，为0表示非注释行
+	int flag=0;          //flag为1表示出现'/'
+	while(!feof(f))
+	{
+		c=fgetc(f);
+		if(chcount<=1)
+		{
+			if(c=='\n')
+			{
+				if(tag==1)
+					commentline++;
+				tag=0;
+				flag=0;
+				chcount=0;
+			}
+			else if(c=='/')
+			{
+				if(flag==0)
+					flag=1;
+				else
+					tag=1;
+			}
+			else if(c!=' '&&c!='\t')
+			{
+				chcount++;
+				flag=0;
+			}
+			else flag=0;
+		}
+		else
+		{
+			if(c=='\n')
+			{
+				tag=0;
+				flag=0;
+				chcount=0;
+			}
+		}
+	}
+	if(tag==1)           //判断最后一行是否为注释行
+		commentline++;
+	fclose(f);
+	printf("The number of commentline is %d.\n",commentline);
 }
 
 int main(int argc,char* argv[])         //在命令行中输入赋予参数具体的值
@@ -71,6 +188,12 @@ int main(int argc,char* argv[])         //在命令行中输入赋予参数具体的值
 		WordCount(fp);
 	else if(!strcmp(argv[1],"-l"))
 		LineCount(fp);
+	else if(!strcmp(argv[1],"-a"))
+	{
+		CodeLineCount(fp);
+		BlankLineCount(fp);
+		CommentLineCount(fp);
+	}
 	else printf("Please input the correrct parameter!\n");
 	return 0;
 }
